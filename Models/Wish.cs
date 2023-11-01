4ec593e415ac1a002b8709d34bced646e87af93b
Phan Văn Năng <22dh112283@st.huflit.edu.vn>
2023-11-01 22:42:39 +0700

    FInished backend(wishlist ) chua lam afteruser to wishlist, chua chia case . chua action vao wishlist

---
+++ Models/Wish.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AirBNB_Admin.Models
+{
+    public class Wish
+    {
+        List<WishItem> items = new List<WishItem>();
+        public IEnumerable<WishItem> Items
+        {
+            get { return items; }
+        }
+        public void Add_Room_Cart (Room room, int _quanty = 1) // lay san pham bo vao gio hang
+        {
+            var item = Items.FirstOrDefault(s => s._product.Id_Room == room.Id_Room);
+            if(item == null) // neu rong thi them moi gio hang
+            {
+                items.Add(new WishItem
+                {
+                    _product = room,
+                    _quality = _quanty
+                }); 
+                
+            }
+            else
+            {
+                item._quality += _quanty; // tong so luong trong gio hang dc cong don
+            }
+        }
+        public int Total_quanlity()
+        {
+            return items.Sum(s => s._quality);
+        }
+        public void Remove_WishItem(int id)
+        {
+            items.RemoveAll(s=>s._product.Id_Room==id);
+        }
+
+    }
+    public class WishItem
+    {
+        public Room _product { get; set; }
+        public int _quality { get; set; }
+    }
+  
+}
