69e96b2a984976ddc02077044119c51a7b231b43
Phan Văn Năng <22dh112283@st.huflit.edu.vn>
2023-11-02 14:10:31 +0700

    finished wishlist case login, heart chua lam ( index cua heart, va csss cua action link qua wish list ) chua sua responsive , chua co sua form css error messages

--- Controllers/WishListController.cs
+++ Controllers/WishListController.cs
@@ -18,34 +18,44 @@ namespace AirBNB_Admin.Controllers
         //}
         public ActionResult ShowWish()
         {
-            if (Session["Wish"] == null)
+            try
             {
-                return RedirectToAction("ShowList", "ShoppingCart");
-                //return View("EmptyWish");
-
-
+                if (Session["Wish"] == null)
+                {
+                    return RedirectToAction("ShowList", "ShoppingCart");
+                    //return View("EmptyWish");
+                }
+                Wish wish = Session["Wish"] as Wish;
+                return View(wish);
             }
-            Wish wish = Session["Wish"] as Wish;
-            return View(wish);
+            catch
+            {
+                return RedirectToAction("Index", "Home");
+            }
+         
         }
         public Wish GetWish()
         {
-            Wish wish = Session["Wish"] as Wish;
-            if(wish == null || Session["Wish"]==null)
-            {
-                wish = new Wish();
-                Session["Wish"] = wish;
-            }
-            return wish;
+          
+                Wish wish = Session["Wish"] as Wish;
+                if (wish == null || Session["Wish"] == null)
+                {
+                    wish = new Wish();
+                    Session["Wish"] = wish;
+                }
+                return wish;
+            
+          
         }
         public ActionResult AddtoWish(int id)
         {
             var _pro = db.Rooms.SingleOrDefault(s => s.Id_Room == id); // lay product theo id
-            if(_pro != null)
+            if(_pro != null && Session["User"]!=null)
             {
                 GetWish().Add_Room_Cart(_pro);
             }
-            return RedirectToAction("ShowWish", "WishList");
+            //return RedirectToAction("ShowWish", "WishList");
+            return RedirectToAction("Index", "Home");
         }
         public ActionResult RemoveWish(int id) // chua xoa dc
         {
