98b242630b5e1523cbbfd509c491458af63512e5
Nguyễn Hoàng Phong <22dh112737@st.huflit.edu.vn>
2023-11-04 21:50:28 +0700

    order

--- Controllers/OrderProductController.cs
+++ Controllers/OrderProductController.cs
@@ -13,9 +13,18 @@ namespace AirBNB_Admin.Controllers
         AirbnbEntities2 db = new AirbnbEntities2();
         public ActionResult Index_OrderProduct(int id)
         {
+            var x = db.Rooms.Where(s => s.Id_Room == id).FirstOrDefault();
+            if (x == null)
+            {
+                Session["event"] = null;
+            }
+            else
+            {
+                Session["event"] = x;
+            }
             Rooms rooms = new Rooms();
             rooms.Id_Room=id;
-            return View(db.Rooms.Where(x=>x.Id_Room==id).ToList());
+            return View(db.Rooms.Where(s=>s.Id_Room==id).ToList());
         }
     }
 }
