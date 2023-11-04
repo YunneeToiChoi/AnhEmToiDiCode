4428a69191805d736c3680dbc071ac5969976757
Te1ne <nq2019thaigiabao230504@gmail.com>
2023-11-05 01:16:20 +0700

    update

---
+++ Controllers/UserMobileController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AirBNB_Admin.Controllers
+{
+    public class UserMobileController : Controller
+    {
+        // GET: UserMobile
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Info()
+        {
+            return View();
+        }
+
+    }
+}
