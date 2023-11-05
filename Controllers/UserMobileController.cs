6294db2119edc44d5d5ce494e0c4ba02290a2ae8
Phan Văn Năng <22dh112283@st.huflit.edu.vn>
2023-11-05 20:21:32 +0700

    finished logic user pc,mb,tablet

--- Controllers/UserMobileController.cs
+++ Controllers/UserMobileController.cs
@@ -1,6 +1,7 @@
 ﻿using AirBNB_Admin.Models;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -21,6 +22,49 @@ namespace AirBNB_Admin.Controllers
         {
             id = 0;
             return View(db.User.Where(x => x.ID_User == id).ToList());
+        }
+        public ActionResult User_Delete(int id)
+        {
+            return View(db.User.Where(s => s.ID_User == id).FirstOrDefault());
+        }
+        [HttpPost]
+        public ActionResult User_Delete(int id, User user)
+        {
+            user = db.User.Where(s => s.ID_User == id).FirstOrDefault();
+            db.User.Remove(user);
+            db.SaveChanges();
+            return RedirectToAction("Logout", "User");
+        }
+        public ActionResult User_Edit(int id)
+        {
+            return View(db.User.Where(s => s.ID_User == id).FirstOrDefault());
+        }
+        [HttpPost]
+        public ActionResult User_Edit(int id, User user)
+        {
+            try
+            {
+                // mot them anh  vao nha 
+                //if (room.UploadImage != null)
+                //{
+                //    string filename = Path.GetFileNameWithoutExtension(room.UploadImage.FileName);
+                //    string extent = Path.GetExtension(room.UploadImage.FileName);
+                //    filename += extent;
+                //    room.Images_Room = "~/Content/image/" + filename;
+                //    room.UploadImage.SaveAs(Path.Combine(Server.MapPath("~/Content/image/"), filename));
+                //}
+                db.Entry(user).State = System.Data.Entity.EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Logout","User");
+            }
+            catch
+            {
+                return Content("Sai roi bro, xoa di ma lam nguoi");
+            }
+        }
+        public ActionResult User_Control(int id)
+        {
+            return View(db.User.Where(x=> x.ID_User==id).ToList());
         }
 
     }
