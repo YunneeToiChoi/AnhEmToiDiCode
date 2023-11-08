9957b9b44834a5ce1733f2efb23acb289665f851
Phan Văn Năng <22dh112283@st.huflit.edu.vn>
2023-11-08 17:37:05 +0700

    xong event filter. ( CO BUG )

---
+++ Scripts/scripts/FilterBox.js
@@ -0,0 +1,9 @@
+﻿var boxfilterforuser = document.querySelector('.vadition__box') // bat cai box mo
+var form = document.querySelector('.form-vadition--modal') // bat form
+var filterbox = document.querySelector('.box-loc') // cai box filter
+
+filterbox.addEventListener('click', moboxfilter )
+function moboxfilter() {
+    form.classList.add('active')
+    boxfilterforuser.classList.add('active')
+}
