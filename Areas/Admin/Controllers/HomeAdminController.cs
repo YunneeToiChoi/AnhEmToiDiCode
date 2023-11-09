d3a96dfe55a31ed2df0024b9f1efd5fccdf2e47b
Nguyễn Hoàng Phong <22dh112737@st.huflit.edu.vn>
2023-11-09 07:11:52 +0700

    xong admin toi qua chợp mắt xíu dậy thấy 4 rưỡi sáng

--- Areas/Admin/Controllers/HomeAdminController.cs
+++ Areas/Admin/Controllers/HomeAdminController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AirBNB_Admin.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -17,6 +18,32 @@ namespace AirBNB_Admin.Areas.Admin.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+        AirbnbEntities2 db = new AirbnbEntities2();
+        public ActionResult LoginAdmin()
+        {
+            return PartialView();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult LoginAdmin(AdminUser user)
+        {
+            var check = db.AdminUser.Where(s => s.Email_User == user.Email_User && s.Password_User == user.Password_User).FirstOrDefault();
+            if (check == null)
+            {
+                ViewBag.LoginFail = "Dang nhap that bai";
+                ModelState.AddModelError("myError", "InvalidEmail or Password");
+                return RedirectToAction("LoginAdmin", "LoginAdmin");
+
+            }
+            else
+            {
+                db.Configuration.ValidateOnSaveEnabled = false;
+                Session["ID"] = user.ID;
+                Session["PasswordUser"] = user.Password_User;
+                db.SaveChanges();
+                return View("localhost:44371/Admin/HomeAdmin/Index");
+            }
         }
     }
 }
